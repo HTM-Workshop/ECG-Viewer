3b30144e72971215fda5f12cea8df6fc4c6bf150
Kevin Williams <williams.kevin@spcollege.edu>
2022-08-02 14:31:20 -0400

    Quickfix for batch files

--- make.bat
+++ make.bat
@@ -1,6 +1,7 @@
 @echo off
 type ecg_viewer.py > ecg_viewer.pyw
 pip3 install -r requirements.txt
+pip3 install pyinstaller
 pyuic5 ecg_viewer_window.ui > ecg_viewer_window.py
 pyinstaller --clean --onefile --icon=icon/icon.png ecg_viewer.pyw
 del /f /q ecg_viewer.pyw
