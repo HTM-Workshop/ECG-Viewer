8bb0a84ce6ec5595f513826091b62d8f7b2fa437
Kevin Williams <williams.kevin@spcollege.edu>
2022-08-17 13:14:17 -0400

    Beta version bump

--- ecg_viewer.py
+++ ecg_viewer.py
@@ -43,7 +43,7 @@ import images_qr        # required for icon to work properly
 
 
 # String used in the title-bar and about window
-VERSION = "v2.0.0-b.3"
+VERSION = "v2.0.0-b.4"
 
 
 # About window. The class is so tiny it might as well be defined here.
